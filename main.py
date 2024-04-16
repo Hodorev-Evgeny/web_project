ba389786cddb92a7f430a0efea620360838ec1f2
User Evgeny <hodorevevgenij6@gmail.com>
2024-04-16 19:51:59 +0600

    account_setting

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ que = """SELECT users.name
 data = list(cur.execute(que))
 message = []
 name_users = 'Me'
+user_id = '1'
 
 
 @app.route('/')
@@ -31,9 +32,26 @@ def home():
 def settings():
     return flask.render_template('settings.html')
 
+@app.route('/settings_account', methods=['GET', 'POST'])
+def settings_account():
+    user_name = request.values.get('username')
+    email = request.values.get('email')
+    user_id = 1
+    con = sqlite3.connect('db/data.db')
+    cur = con.cursor()
+
+    if user_name is not None and email is not None:
+        qur = """UPDATE users
+                SET name = ?,
+                email = ?
+                WHERE id = ?"""
+        cur.execute(qur, (user_name, email, user_id))
+        return flask.render_template('chat2.html')
+
+    return flask.render_template('set.html')
 
 
 if __name__ == '__main__':
     db_session.global_init('db/data.db')
-    app.run(port=20000, host='127.0.0.1')
+    app.run(port=800, host='127.0.0.1')
 
